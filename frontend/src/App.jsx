5a8d72eb8452ee69be6c5ba7e4a83d996118fd50
sarayu <sarayu.vijayanagarams@gmail.com>
2025-09-19 22:12:34 -0400

    remove extra header

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,9 +5,6 @@ function App() {
   return (
     <div className="App">
       <h1>Fred's Fridge</h1>
-      <div className='fridge'>
-        <h2>Fridge</h2>
-      </div>
       <ShoppingList />
     </div>
   );
