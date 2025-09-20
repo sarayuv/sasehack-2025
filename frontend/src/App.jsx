10d5aef07bbf68be89bf8e27739c58e945d33834
sarayu <sarayu.vijayanagarams@gmail.com>
2025-09-19 23:25:25 -0400

    improve shopping list design

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,6 +5,7 @@ import './App.css';
 
 function App() {
   const [shoppingList, setShoppingList] = useState([]);
+  const [isModalOpen, setIsModalOpen] = useState(false);
 
   const addItem = (newItem) => {
     if (newItem.trim()) {
@@ -15,12 +16,16 @@ function App() {
   return (
     <div className="App">
       <h1>Fred's Fridge</h1>
-      <ul>
-        {shoppingList.map((item, index) => (
-          <li key={index}>{item}</li>
-        ))}
-      </ul>
-      <ShoppingList shoppingList={shoppingList} addItem={addItem} />
+      <div className='main-shopping-list'>
+        <h2>Shopping List</h2>
+        <ul>
+          {shoppingList.map((item, index) => (
+            <li key={index}>{item}</li>
+          ))}
+        </ul>
+        <button className='open-shopping-list-button' onClick={() => setIsModalOpen(true)}>Open Shopping List</button>
+      </div>
+      <ShoppingList shoppingList={shoppingList} addItem={addItem} isModalOpen={isModalOpen} setIsModalOpen={setIsModalOpen} />
       <Reminders />
 
       <div style={{ height:'210vh'}}></div>
--- frontend/src/components/ShoppingList.jsx
+++ frontend/src/components/ShoppingList.jsx
@@ -1,9 +1,8 @@
 import React, {useState, useEffect} from 'react';
 import '../styles/ShoppingList.css';
 
-function ShoppingList({shoppingList, addItem}) {
+function ShoppingList({shoppingList, addItem, isModalOpen, setIsModalOpen}) {
   const [newItem, setNewItem] = useState("");
-  const [isModalOpen, setIsModalOpen] = useState(false);
 
   const handleAddItem = () => {
     addItem(newItem);
@@ -13,8 +12,6 @@ function ShoppingList({shoppingList, addItem}) {
 
   return (
       <div className='shopping-list'>
-        <button onClick={() => setIsModalOpen(true)}>Open Shopping List</button>
-
         {isModalOpen && (
           <div className='modal'>
             <div className='modal-content'>
