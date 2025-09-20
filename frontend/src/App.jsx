ccc3d5bd0d0b893b1bfff6c047f899689a9e7203
Abhi <ayammanuru@wpi.edu>
2025-09-19 23:13:43 -0400

    mkinda fixed some things with the reminders

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,43 @@
+import React, {useState} from 'react';
 import ShoppingList from './components/ShoppingList';
 import Reminders from './components/Reminders';
 import './App.css';
+import ReminderList from './components/Reminders';
 
 function App() {
+  const [shoppingList, setShoppingList] = useState([]);
+
+  const addItem = (newItem) => {
+    if (newItem.trim()) {
+      setShoppingList((prevList) => [...prevList, newItem]);
+    }
+  };
+
+  const [reminderList, setReminderList] = useState([]);
+
+  const addReminder = (newReminder) => {
+    if (newReminder.trim()) {
+      setReminderList((prevList) => [...prevList, newReminder]);
+    }
+  };
+
   return (
     <div className="App">
       <h1>Fred's Fridge</h1>
-      <div className='fridge'>
-        <h2>Fridge</h2>
-      </div>
       <ShoppingList />
-      <Reminders />
+      <ul>
+        {shoppingList.map((item, index) => (
+          <li key={index}>{item}</li>
+        ))}
+      </ul>
+      <ShoppingList shoppingList={shoppingList} addItem={addItem} />
+      <ReminderList />
+      <ul>
+        {reminderList.map((reminder, index) => (
+          <li key={index}>{reminder}</li>
+        ))}
+      </ul>
+      <ReminderList reminderList={reminderList} addReminder={addReminder} />
     </div>
   );
 }
